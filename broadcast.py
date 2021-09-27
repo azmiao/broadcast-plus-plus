eccd0527a9d634d5b220c6559852ca046006880d
AZMIAO <54100327+azmiao@users.noreply.github.com>
2021-09-27 07:38:26 -0500

    v1.1    新增反选机制  close #1

--- broadcast.py
+++ broadcast.py
@@ -8,18 +8,18 @@ from hoshino.service import Service
 lock = asyncio.Lock()
 broadcast_record = []
 
-#格式：[bc|广播] [服务名|g-群号/群号/群号] 广播内容
+#格式：[bc|广播] [服务名 | g-群号/群号/群号 | exg-群号/群号/群号] 广播内容
 @sucmd('broadcast', aliases=('bc', '广播'))
 async def broadcast(session: CommandSession):
     msg = session.current_arg
     if not ' ' in msg:
-        await session.send(f'请输入服务名，全群广播则输入all，分群广播请以"g-"开头后接QQ群号，并以逗号相隔')
+        await session.send(f'请输入服务名，全群广播则输入all，分群广播请以"g-"开头后接QQ群号，并以逗号相隔，若排除性广播请用"eg-"开头')
         return
     args = msg.split(' ',1)
     bc_sv_name =  args[0]
     bc_msg = args[1]
     svs = Service.get_loaded_services()
-    if bc_sv_name not in svs and bc_sv_name != 'all' and not bc_sv_name.startswith('g-'):
+    if bc_sv_name not in svs and bc_sv_name != 'all' and not bc_sv_name.startswith('g-') and not bc_sv_name.startswith('exg-'):
         await session.send(f'未找到该服务，请输入正确的服务')
         return
     sid = list(hoshino.get_self_ids())[0]
@@ -29,6 +29,13 @@ async def broadcast(session: CommandSession):
     elif bc_sv_name.startswith('g-'):
         bc_l = bc_sv_name.replace('g-', '')
         gl = bc_l.split(r'/')
+    elif bc_sv_name.startswith('exg-'):
+        gl = await session.bot.get_group_list(self_id=sid)
+        gl = [ g['group_id'] for g in gl ]
+        bc_l = bc_sv_name.replace('exg-', '')
+        exclude_gl = bc_l.split(r'/')
+        for ex_gid in exclude_gl:
+            gl.remove(int(ex_gid))
     else:
         enable_groups = await svs[bc_sv_name].get_enable_groups()
         gl = enable_groups.keys()
